775c91c391ae7b700f6f319ec4dd09ea3dfbc3f6
Lany Aragão <lanyaragao5@gmail.com>
2022-06-30 16:08:59 -0300

    Subindo projeto WPF Calibur

---
+++ wpfapp/Bootstrapper.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using Caliburn.Micro;
+using wpfapp.ViewModels;
+
+
+namespace wpfapp
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            base.Initialize();
+        }
+        protected override void OnStartup(object sender, StartupEventArgs e)
+
+        {
+            base.DisplayRootViewFor<MainViewModel>();
+        }
+    }
+}
---
+++ wpfapp/ViewModels/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Caliburn.Micro;
+
+namespace wpfapp.ViewModels
+{
+    public class MainViewModel : Screen
+    {
+        private string nomeCompleto;
+
+        public string NomeCompleto
+        {
+            get
+            {
+                return nomeCompleto;
+            }
+            set
+            {
+                nomeCompleto = value;
+                base.NotifyOfPropertyChange(() => NomeCompleto);
+            }
+        }
+    }
+}
